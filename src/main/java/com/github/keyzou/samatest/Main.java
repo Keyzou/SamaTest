4a803785101edfb2599823e027df10bf464845aa
Keyzou <keyzou95@gmail.com>
2016-03-05 18:42:43 +0100

    On fait des tests

--- src/main/java/com/github/keyzou/samatest/Main.java
+++ src/main/java/com/github/keyzou/samatest/Main.java
@@ -1,5 +1,7 @@
 package com.github.keyzou.samatest;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -7,6 +9,15 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable(){
         getLogger().info("Ca marche");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+        if(cmd.getName().equalsIgnoreCase("test")){
+            sender.sendMessage("Bien ouej");
+            return true;
+        }
+        return false;
     }
 
 
